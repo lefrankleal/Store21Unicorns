1271c622213dd385c0a2bffbe8c64f0f09a39d47
Frank Leal <falg2@hotmail.com>
2020-07-04 00:21:28 -0500

    config index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-/**
- * @format
- */
+import { App } from './src/App'
+import { AppRegistry } from 'react-native'
+import { name as appName } from './app.json'
 
-import App from './src/App';
-import { AppRegistry } from 'react-native';
-import { name as appName } from './app.json';
-
+console.disableYellowBox = true
 AppRegistry.registerComponent(appName, () => App);
