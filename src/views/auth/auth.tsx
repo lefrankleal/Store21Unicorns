99504ce469ea49a37addfefff24d51f608d87cbe
Frank Leal <falg2@hotmail.com>
2020-07-04 00:26:49 -0500

    anuth and login view

---
+++ src/views/auth/auth.tsx
@@ -0,0 +1,11 @@
+import { View } from 'react-native'
+
+const Auth = () => {
+  return (
+    <View>
+      <Text>Auth</Text>
+    </View>
+  )
+}
+
+export default Auth
---
+++ src/views/auth/login.tsx
@@ -0,0 +1,15 @@
+import { View } from 'react-native'
+
+interface LoginProps extends ComponentProps {
+
+}
+
+const Login = (props: LoginProps) => {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  )
+}
+
+export default Login
