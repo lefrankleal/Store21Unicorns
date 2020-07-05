84bd6c3573c52dcf25d920bcf41096a7f7a6f5cf
Frank Leal <falg2@hotmail.com>
2020-07-04 23:58:02 -0500

    product view

--- src/views/app/Product.tsx
+++ src/views/app/Product.tsx
@@ -1,16 +1,21 @@
-import { ComponentProps } from 'react'
+import React, { ComponentProps } from 'react'
+
 import { ScrollView } from 'react-native'
 import { Text } from 'react-native-elements'
+import { useRoute } from '@react-navigation/native'
 
 interface ProductProps extends ComponentProps {
 
 }
 
 const Product = (props: ProductProps) => {
+  const route = useRoute()
+  console.log(route.params)
+
   return (
     <ScrollView
       contentInsetAdjustmentBehavior="automatic">
-        <Text>Product</Text>
+      <Text>Product</Text>
     </ScrollView>
   )
 }
