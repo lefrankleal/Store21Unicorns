9081e525bbe63e4ff81357bfb54e35194add23ae
Frank Leal <falg2@hotmail.com>
2020-07-05 04:26:32 -0500

    typo fix

--- android/app/src/main/java/com/store21uniconrns/MainActivity.java
+++ android/app/src/main/java/com/store21uniconrns/MainActivity.java
@@ -1,4 +1,4 @@
-package com.store21uniconrns;
+package com.store21unicorns;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Store21Uniconrns";
+    return "Store21Unicorns";
   }
 }
--- ios/Store21UnicornsTests/Store21UnicornsTests.m
+++ ios/Store21UnicornsTests/Store21UnicornsTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface Store21UniconrnsTests : XCTestCase
+@interface Store21UnicornsTests : XCTestCase
 
 @end
 
-@implementation Store21UniconrnsTests
+@implementation Store21UnicornsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
